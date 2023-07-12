29d064bede299a7fd58841acbf0bb686a4ba7362
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-12 18:48:41 +0200

    Create new tiles w/ classes of 'blank' and 'tile'

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function createGrid(gridSideSize) {
   if (gridSideSize > 100) {
     gridSideSize = 100;
   }
+
   const gridContainer = document.querySelector('.grid-container');
 
   for (let i = 1; i <= gridSideSize; i++) {
     for (let j = 1; j <= gridSideSize; j++) {
 
       const newTile = document.createElement('div');
-      newTile.setAttribute('class', 'blank-tile');
+      newTile.classList.add('blank', 'tile');
       newTile.setAttribute('id', `tile_${i}.${j}`);
 
       newTile.style.gridRow = i;
