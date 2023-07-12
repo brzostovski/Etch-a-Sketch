23c7be53369e24b8e7b951c2e8fd184fd942f036
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-12 18:16:23 +0200

    Redo createGrid function

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function createGrid(gridSideSize) {
+
   const gridContainer = document.querySelector('.grid-container');
+
   for (let i = 1; i <= gridSideSize; i++) {
-
-    const newRow = document.createElement('div');
-    newRow.setAttribute('class', 'row');
-    newRow.setAttribute('id', `row_${i}`);
-    gridContainer.appendChild(newRow);
-
     for (let j = 1; j <= gridSideSize; j++) {
 
       const newTile = document.createElement('div');
       newTile.setAttribute('class', 'blank-tile');
       newTile.setAttribute('id', `tile_${i}.${j}`);
-      newRow.appendChild(newTile);
+
+      newTile.style.gridColumn = i;
+      newTile.style.gridRow = j;
+
+      gridContainer.appendChild(newTile);
     }
   }
 }
 
-createGrid(4);
+createGrid(5);
